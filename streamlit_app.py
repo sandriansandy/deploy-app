208f4bc1195c23619d51d660c32e65af12a7c229
sandriansandy <76563461+sandriansandy@users.noreply.github.com>
2022-12-13 20:34:16 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,8 @@
 import streamlit as st
-st.title('Welcome to Evoke Technologies')
+import streamlit as st
 
-st.header('This is a header')  # same goes with st.subheader('xxxxxxx')
+def main():
+    st.title("Sandy Ganteng")
 
-st.text('This is used to display simple text')
-
-st.success('This is used to display Success messages in a green box')
-
-st.warning('This is used to display Warning message in orange box')
-
-st.error('This is used to display Error messages in red box')
-
-st.info('This is used to display Information in a blue box')
+if __name__=='__main__':
+    main()
