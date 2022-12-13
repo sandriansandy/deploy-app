37556411f343922dbc46ec11ab6d2c1841ca71b8
sandriansandy <76563461+sandriansandy@users.noreply.github.com>
2022-12-13 21:06:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,22 @@
 import streamlit as st
-import streamlit as st
 
+model = tf.keras.models.load_model('model_ann.h5')
 def main():
     st.title("Sandy Ganteng")
+    st.header('Prediksi Sentimen dan Aspek Ulasan')
+    text = st.text_input('Masukkan Teks Ulasan', 'Makanan enak sekali')
+    if st.button('Mulai Analisis'):
+        if text.strip()=='' :
+            st.error('Cek kembali teks ulasan', icon="🚨")
+        else:
+            with st.spinner('Wait for it...'):
+                time.sleep(0.1)
+                st.success('Success!')
+
+            teks_df = model.predict(text)
+            st.table(teks_df)
+    else:
+        st.write('')
 
 if __name__=='__main__':
     main()
