45fe53a84ec3a0aae55e938d6dd96c6921ff7b07
sandriansandy <76563461+sandriansandy@users.noreply.github.com>
2022-12-14 13:38:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,10 +7,6 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 model = tf.keras.models.load_model('model_ann.h5')
 def main():
-    vocab_size = 10000
-    max_length = 75
-    trunc_type='post'
-    oov_tok = "<OOV>"
     st.title("Sandy Ganteng")
     st.header('Prediksi Sentimen Ulasan New Star Cineplex Pasuruan')
     text = st.text_input('Masukkan Teks Ulasan', 'Bioskop sangat nyaman')
@@ -22,9 +18,9 @@ def main():
                 time.sleep(0.1)
                 st.success('Success!')
             
-            tokenizer = Tokenizer(num_words = vocab_size, oov_token=oov_tok)    
+            tokenizer = Tokenizer(num_words = 10000, oov_token="<OOV>")    
             sequences_input = tokenizer.texts_to_sequences(text)
-            padded_input = pad_sequences(sequences_input,maxlen=max_length, truncating=trunc_type)
+            padded_input = pad_sequences(sequences_input,maxlen=75, truncating="post")
             teks_df = model.predict(padded_input)
             rounded = [np.round(x) for x in predicted]
             for i in rounded:
