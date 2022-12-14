1eb19d2bdc780cff4183094241f6bc81ba79a5e4
sandriansandy <76563461+sandriansandy@users.noreply.github.com>
2022-12-14 15:49:54 +0700

    Update predict.py

--- predict.py
+++ predict.py
@@ -4,18 +4,12 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 import tensorflow as tf
 import numpy as np
 
-
-def process_input(input):
+def predict(teks):
     tokenizer = Tokenizer(num_words=10000, oov_token="<OOV>")
     input_list = []
-    input_list.append(input)
+    input_list.append(teks)
     sequences_input = tokenizer.texts_to_sequences(input_list)
-    padded_input = pad_sequences(sequences_input, maxlen=75, truncating='pre')
-    return padded_input
-
-
-def predict(teks):
-    processed_input = process_input(teks)
+    padded_input = pad_sequences(sequences_input, maxlen=75, truncating='post')
     model = tf.keras.models.load_model(
         open(os.path.basename('model_ann.h5'), 'rb'))
     predicted = model.predict(processed_input)
