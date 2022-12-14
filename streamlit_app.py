7aba2eed97e078c449042d5e4860a2492cc95b27
sandriansandy <76563461+sandriansandy@users.noreply.github.com>
2022-12-14 14:03:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import time
 import numpy as np
 from tensorflow.keras.preprocessing.text import Tokenizer
-from keras.utils import pad_sequences
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 model = tf.keras.models.load_model('model_ann.h5')
 def main():
