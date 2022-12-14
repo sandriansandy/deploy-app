b627f72d731ded22c4a5a5ecf2d6105c5b5ad9c8
sandriansandy <76563461+sandriansandy@users.noreply.github.com>
2022-12-14 16:40:32 +0700

    Update predict.py

--- predict.py
+++ predict.py
@@ -19,8 +19,9 @@ def predict(teks):
 
     rounded = np.round(predicted)
     for i in rounded:
-        if i[0] == 0:
-            return ("Sentimen Positif")
-        elif i[0] == 1:
-            return ("Sentimen Negatif")
-        break    
+        return i[0]
+#         if i[0] == 0:
+#             return ("Sentimen Positif")
+#         elif i[0] == 1:
+#             return ("Sentimen Negatif")
+#         break    
