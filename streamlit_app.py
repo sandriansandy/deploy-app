ad8e49ead998f02e70e15f9d3ed9e5c15a6216c5
sandriansandy <76563461+sandriansandy@users.noreply.github.com>
2022-12-14 15:40:31 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,37 +1,30 @@
 import streamlit as st
 import tensorflow as tf
 import time
-import numpy as np
 from tensorflow.keras.preprocessing.text import Tokenizer
 from keras.utils import pad_sequences
+import predict
 
 model = tf.keras.models.load_model('model_ann.h5')
+
+
 def main():
-    st.title("Sandy Ganteng")
     st.header('Prediksi Sentimen Ulasan New Star Cineplex Pasuruan')
     text = st.text_input('Masukkan Teks Ulasan', 'Bioskop sangat nyaman')
     if st.button('Mulai Analisis'):
-        if text.strip()=='' :
+        if text.strip() == '':
             st.error('Cek kembali teks ulasan', icon="🚨")
         else:
             with st.spinner('Wait for it...'):
                 time.sleep(0.1)
                 st.success('Success!')
-            
-            tokenizer = Tokenizer(num_words = 10000, oov_token="<OOV>")    
-            sequences_input = tokenizer.texts_to_sequences(text)
-            padded_input = pad_sequences(sequences_input, maxlen=75, truncating='post', dtype='int32')
-            predicted = model.predict(padded_input)
-            rounded = [np.round(x) for x in predicted]
-            for i in rounded:
-                if i == 1:
-                    print("Sentimen Negatif")
-                else:
-                    print("Sentimen Positif")
-                break
-            #st.table(teks_df)
+
+            processed_input = predict.process_input(text)
+            predict.predict(processed_input)
+            # st.table(teks_df)
     else:
         st.write('')
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
