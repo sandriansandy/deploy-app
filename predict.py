86f65ca6d39b27f41f4776a943d9c938a2bb85f1
sandriansandy <76563461+sandriansandy@users.noreply.github.com>
2022-12-15 19:19:26 +0700

    Update predict.py

--- predict.py
+++ predict.py
@@ -5,28 +5,24 @@ import tensorflow as tf
 import numpy as np
 
 model = tf.keras.models.load_model('model_ann.h5')
-list_teks = []
 word_index=[]
 with open("word_index.txt",'r') as indeks:
     for word in indeks:
         word_index.append(word)
         
 tokenizer = Tokenizer(num_words=10000, oov_token="<OOV>")
-# tokenizer.fit_on_texts(word_index)
+tokenizer.fit_on_texts(word_index)
 
+@st.cache
 def predict(teks):
+    list_teks = []
     list_teks.append(teks)
-    tokenizer.fit_on_texts(list_teks)
     sequences_input = tokenizer.texts_to_sequences(list_teks)
     padded_input = pad_sequences(sequences_input, maxlen=75, truncating='post')
     
     predicted = model.predict(padded_input)
-#     return predicted
-    rounded = np.round(predicted)
-    return rounded
-#     for i in rounded:
-#         if i[0] == 0:
-#             return ("Sentimen Positif")
-#         elif i[0] == 1:
-#             return ("Sentimen Negatif")
-#         break    
+    if np.round(predicted)==0:
+        return ("Sentimen Positif")
+    elif np.round(predicted)==1:
+        return ("Sentimen Negatif")
+    
